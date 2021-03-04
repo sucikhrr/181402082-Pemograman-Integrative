f75eec19e185f57da1c491e57aa3117242090f91
sucikhrr <52316304+sucikhrr@users.noreply.github.com>
2021-03-04 10:45:31 +0700

    penjumlahan

---
+++ tugasPI.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int a= 4, b=2;
+
+int penjumlahan {
+int jumlah ;
+jumlah = a + b ;
+return jumlah ;
+}
+
+int main ()
+{
+cout<<endl<<"Pengurangan a dan b = "<<penjumlahan();
+return 0;
+}
